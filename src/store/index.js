a34671c8fae7fdddf8c25051e101e885ce98cb8e
castrocarlos313 <castrocarlos313@gmail.com>
2022-03-31 23:53:36 -0300

    Bugfix redux

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,6 @@ import thunk from "redux-thunk";
 
 import climaReducer from "../reducers/climaReducer";
 
-const store = createStore(
-  climaReducer,
-  compose(
-    applyMiddleware(thunk),
-    typeof window === "object" && typeof window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
-  )
-);
+const store = createStore(climaReducer, compose(applyMiddleware(thunk)));
 
 export default store;
